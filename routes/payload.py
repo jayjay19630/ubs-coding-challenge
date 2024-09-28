360245a1213511ec1f64993199e3e9a68691cff6
positivelyjon <positivelyjon@gmail.com>
2024-09-28 23:35:02 +0800

    do stack

--- routes/payload.py
+++ routes/payload.py
@@ -4,3 +4,11 @@ from routes import app
 @app.route('/payload_crackme')
 def get_crackme_payload():
     return send_file('payload_crackme')
+
+@app.route('/payload_stack')
+def get_stack_payload():
+    return send_file('payload_stack')
+
+@app.route('/payload_shellcode')
+def get_shellcode_payload():
+    return send_file('payload_shellcode')
